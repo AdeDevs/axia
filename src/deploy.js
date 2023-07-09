a35e00732cd8264533b4e20bbdd3e7f419b14bfb
AdeDevs <adeyemiakinyemi01@gmail.com>
2023-07-09 04:59:46 +0100

    Push

---
+++ src/deploy.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBeH6jd5352A3sf5BR6ekOxQW6yoXu4Gdk",
+  authDomain: "kryptykaxia.firebaseapp.com",
+  projectId: "kryptykaxia",
+  storageBucket: "kryptykaxia.appspot.com",
+  messagingSenderId: "490889150379",
+  appId: "1:490889150379:web:3488d76032dce69241dcee"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
